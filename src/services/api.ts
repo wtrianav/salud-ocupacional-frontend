3274208b945761aa7c7b814efc4b54bd37e9ee51
Andrés Triana <wtriana@outlook.com>
2026-06-16 11:35:21 -0500

    refactor: se elimina el registro de depuración del interceptor de peticiones de Axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,6 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
 	const token = localStorage.getItem('token');
-	console.log("Interceptor Axios - Token actual:", token);
 	if (token) {
 		config.headers.Authorization = `Bearer ${token}`;
 	}
